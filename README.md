438421865ab8868867db38305e26b0373f1f3d8d
Erick Brandao <brandaoerick@MacBook-Air.local>
2023-04-02 19:09:06 -0300

    added: comments to the app logic & created a README.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# image-carousel
+# Image Carousel
+This is a simple HTML app that displays an image carousel with four images. The carousel includes two buttons: "Prev" and "Next" to navigate through the images.
+
+### Installation
+There is nothing to install, you just need to download the files and open the index.html file in your web browser.
+
+### Usage
+To use the app, click on the "Next" or "Prev" button to navigate through the images. 
+The images will automatically slide every two seconds.
--- script.js
+++ script.js
@@ -3,35 +3,46 @@ const leftBtn = document.getElementById("left");
 const rightBtn = document.getElementById("right");
 
 const img = document.querySelectorAll("#imgs img");
+
 let index = 0;
 
+// Change the displayed image
 const changeImage = () => {
+  // If the index is greater than the number of images, reset it to 0
   if (index > img.length - 1) {
     index = 0;
-  } else if (index < 0) {
+  } 
+  // If the index is less than 0, set it to the last image
+  else if (index < 0) {
     index = img.length - 1;
   }
 
+  // Move the image container horizontally using CSS transform property
   imgs.style.transform = `translateX(${-index * 500}px)`;
 };
+
 const run = () => {
   index++;
   changeImage();
 };
 
+// Set the interval for the carousel to run every 2 seconds
 let interval = setInterval(run, 2000);
 
+// Reset the interval when a button is clicked
 const resetInterval = ()=> {
   clearInterval(interval);
   interval = setInterval(run, 2000);
 }
 
+// Change the image to the next one
 rightBtn.addEventListener("click", () => {
   index++;
   changeImage();
   resetInterval();
 });
 
+// Change the image to the previous one
 leftBtn.addEventListener("click", () => {
   index--;
   changeImage();
